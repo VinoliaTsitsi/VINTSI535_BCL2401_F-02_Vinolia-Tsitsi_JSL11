9473c486766a4cd65117eaaefba060f07a1316b5
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-04-25 06:09:29 +0200

    got the Elements from the DOM

--- index.js
+++ index.js
@@ -18,8 +18,17 @@ function initializeData() {
 initializeData()
 // TASK: Get elements from the DOM
 const elements = {
+headerBoardName: document.getElementById('header-board-name'),
+columnDivs: document.querySelectorAll('column-div'),
+filterDiv: document.getElementById('filterDiv'), 
+hideSideBarBtn: document.getElementById('hide-side-bar-btn'), 
+showSideBarBtn: document.getElementById('show-side-bar-btn'), 
+themeSwitch: document.getElementById('switch'), 
+createNewTaskBtn: document.getElementById('create-task-btn'), 
+modalWindow: documen.querySelector('.modal-window'), 
+editTaskModal: document.querySelector('.edit-task-modal-window')
 
-}
+}; 
 
 let activeBoard = ""
 
